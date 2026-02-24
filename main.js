c468fde33d316a874849148d59b974c4f11529fb
samueleee1016 <samuele.mastrovincenzo@gmail.com>
2026-02-24 23:03:32 +0100

    fixed a problem in main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+app.set('trust proxy', 1);
 const session = require('express-session');
 const http = require('http');
 const path = require('path');
