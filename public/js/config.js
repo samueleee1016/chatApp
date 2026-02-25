cdce376c9e91a54806cfe2117784e102791e293a
samueleee1016 <samuele.mastrovincenzo@gmail.com>
2026-02-25 10:02:46 +0100

    fixed problem in config.js for deploy

--- public/js/config.js
+++ public/js/config.js
@@ -3,5 +3,5 @@ const _wsProtocol = _protocol === 'https:' ? 'wss:' : 'ws:';
 const _host = window.location.host;
 
 const HTTP_BASE = `${_protocol}//${_host}`;
-const API_BASE = `${HTTP_BASE}`;
+const API_BASE = `${HTTP_BASE}/chatApp`;
 const WS_BASE = `${_wsProtocol}//${_host}`;
